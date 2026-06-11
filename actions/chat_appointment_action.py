f7f91b38916fb85ec125ea796731bf83d6be8787
Muhindhar <muhindharsv27@gmail.com>
2026-06-11 13:04:07 +0530

    added chat feature

---
+++ actions/chat_appointment_action.py
@@ -0,0 +1,38 @@
+
+from actions.base_action import BaseAction
+from pages.chat_appointment_pages import Chatpage
+from utilities.logger import get_logger
+logger = get_logger()
+class Chataction(BaseAction):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.cap = Chatpage()
+    
+    def clickrecp(self):
+        logger.info("clicking receptionist")
+        self.click(self.cap.recbtn)
+
+    def clicksignin(self):
+        logger.info("clicking signin")
+        self.click(self.cap.signin)
+
+    def clickappointment(self):
+        logger.info("clicking appointment section")
+        self.click(self.cap.appbtn)
+
+    def clickchaticon(self):
+        logger.info("clicking chat icon")
+        self.click(self.cap.chatbtn)
+
+    def selectdoctor(self):
+        logger.info("selecting doctor")
+        self.click(self.cap.person)
+
+    def sendmessage(self):
+        logger.info("sending message")
+        self.send_keys(self.cap.msg,"Hello Doctor")
+        self.click(self.cap.sendmsg)
+
+    def verifymessage(self):
+        logger.info("verifying message")
+        return self.is_displayed(self.cap.check)
