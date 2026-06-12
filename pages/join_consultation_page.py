bf4092e2784894c3a2cc6bce7ad5fb305b50b88d
VIGNESHWARAN-USER <vigneshwaran.coder@gmail.com>
2026-06-12 05:44:49 +0530

    Added action page for consultation and Modified the locators

--- pages/join_consultation_page.py
+++ pages/join_consultation_page.py
@@ -1,30 +1,29 @@
 from selenium.webdriver.common.by import By
 
 class JoinConsultationPage:
-    tableBody = (By.XPATH, "//div/table/tbody/tr")
-    popUp = (By.XPATH, "//div[@id = \"modal-chkstatus\"]")
-    startNow = (By.XPATH, "//a[contains(@href , \"zoom\") and @class = \"btn btn-outline-success btn-sm pull-right\"]")
-    addButton = (By.XPATH, "//div[@class = \"box-tools pull-right box-tools-md\"]/button[1]")
-    patientName = (By.XPATH, "//select[@id = \"addpatient_id\"]/following-sibling::span/span/span/span[2]")
-    firstPatientoption = (By.XPATH, "//span[@class = \"select2-results\"]/ul/li[1]")
-    patientNameInput =(By.CSS_SELECTOR, "input.select2-search__field")
-    title = (By.XPATH, "//input[@name = \"title\"]")
-    date = (By.CSS_SELECTOR, "input#datetimepicker")
-    duration = (By.CSS_SELECTOR, "input#duration")
-    ipdOrOpd = (By.XPATH, "//select[@class = \"form-control module_type\"]")
-    description = (By.CSS_SELECTOR, "textarea#description")
-    addCredentialButton = (By.XPATH, "//div[@class= \"box-tools pull-right box-tools-md\"]/button[2]")
-    zoomApiKeyField = (By.XPATH, "//input[@id = \"zoom_api_key\"]")
-    zoomApiSecretField = (By.XPATH, "//input[@id = \"zoom_api_secret\"]")
-    saveCredentialButton = (By.XPATH, "//button[@id = \"submit-btn-credential\"][2]")
-    successToastMessage = (By.XPATH, "//div[@class = \"toast-message\"]")
-    saveConsultationButton = (By.XPATH, "//div[@class = \"pull-right mrminus8\"]/button[@class = \"btn btn-primary\"]")
+    table_body = (By.XPATH, "//div/table/tbody/tr")
+    popup = (By.XPATH, "//div[@id = 'modal-chkstatus']")
+    start_now = (By.XPATH,"//a[contains(@href , 'zoom') and @class = 'btn btn-outline-success btn-sm pull-right']")
+    add_button = (By.XPATH,"//div[@class = 'box-tools pull-right box-tools-md']/button[1]")
+    patient_name = (By.XPATH,"//select[@id = 'addpatient_id']/following-sibling::span/span/span/span[2]")
+    first_patient_option = (By.XPATH,"//span[@class = 'select2-results']/ul/li[1]")
+    patient_name_input = (By.CSS_SELECTOR,"input.select2-search__field")
+    title = (By.XPATH,"//input[@name = 'title']")
+    date = (By.CSS_SELECTOR,"input#datetimepicker")
+    duration = (By.CSS_SELECTOR,"input#duration")
+    ipd_or_opd = (By.XPATH,"//select[@class = 'form-control module_type']")
+    description = (By.CSS_SELECTOR,"textarea#description")
+    add_credential_button = (By.XPATH,"//div[@class = 'box-tools pull-right box-tools-md']/button[2]")
+    zoom_api_key_field = (By.XPATH,"//input[@id = 'zoom_api_key']")
+    zoom_api_secret_field = (By.XPATH,"//input[@id = 'zoom_api_secret']")
+    save_credential_button = (By.XPATH,"//button[@id = 'submit-btn-credential'][2]")
+    success_toast_message = (By.XPATH,"//div[@class = 'toast-message']")
+    save_consultation_button = (By.XPATH,"//div[@class = 'pull-right mrminus8']/button[@class = 'btn btn-primary']")
 
-
-    def getStatusDropdown(self, rowIndex):
+    def get_status_dropdown(self, rowIndex):
         return (By.XPATH, "//tbody/tr[" + rowIndex + "]/td[8]/form/select")
     
 
-    def getActionButtons(self, rowIndex):
+    def get_action_buttons(self, rowIndex):
         return (By.XPATH, "//tbody/tr[" + rowIndex + "]/td[9]/a")
     
--- pages/sidebar_page.py
+++ pages/sidebar_page.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 
 class SideBarPage:
-    liveConsultation = (By.XPATH, "//li[contains(@class ,\"treeview\")][19]")
-    liveConsultationOption = (By.XPATH, "//li[contains(@class ,\"treeview\")][19]/ul/li/a")
+    live_consultation = (By.XPATH, "//li[contains(@class ,\"treeview\")][19]")
+    live_consultation_option = (By.XPATH, "//li[contains(@class ,\"treeview\")][19]/ul/li/a")
     pathlogyMenu = (By.XPATH, "//a[normalize-space()='Pathology']")
     messaging = (By.XPATH,"//a[@href=\"https://demo.smart-hospital.in/admin/notification\"]/child::span");
