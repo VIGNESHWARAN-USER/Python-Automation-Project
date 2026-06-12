a965a8145335b76660d0595784a2427064ad9c19
Mythilysenthil <mythilsenthilp@gamil.com>
2026-06-12 12:12:17 +0530

    Added test for send message feature

--- actions/send_msg_action.py
+++ actions/send_msg_action.py
@@ -1,26 +1,26 @@
 from actions.base_action import BaseAction
-from pages.leavemsg_page import LeaveMsg
+from pages.sendmsg_page import SendMsg
 from pages.sidebar_page import SideBarPage
 from utilities.logger import get_logger
 
 logger = get_logger()
 
-class PathalogyAction(BaseAction):
+class SendMsgAction(BaseAction):
 
     def __init__(self, driver):
         super().__init__(driver)
-        self.lm = LeaveMsg()
+        self.sm = SendMsg()
         self.sp = SideBarPage()
 
     def click_pathologist_login(self):
         try:
-            self.click(self.lm.pathologistBtn)
+            self.click(self.sm.pathologistBtn)
         except Exception as e:
             logger.error(f"failed to login to the pathologist: {str(e)}")        
 
     def click_signin(self):
         try:
-           self.click(self.lm.signInBtn)
+           self.click(self.sm.signInBtn)
         except Exception as e:
             logger.error(f"failed to signup to pathologist: {str(e)}")
 
@@ -32,33 +32,45 @@ class PathalogyAction(BaseAction):
 
     def click_sendSMS(self):
         try:
-            self.click(self.lm.sendSms)
+            self.click(self.sm.sendSms)
         except Exception as e:
             logger.error(f"failed to click the send sms: {str(e)}")
 
     def enter_title(self,title):
         try:
-            self.send_keys(self.lm.title,title)
+            self.send_keys(self.sm.title,title)
         except Exception as e:
             logger.error(f"failed to enter titel: {str(e)}")
 
     def enter_tempid(self, tempid):
         try:
-            self.send_keys(self.lm.tempId,tempid)
+            self.send_keys(self.sm.tempId,tempid)
         except Exception as e:
             logger.error(f"failed to enter template id: {str(e)}")
 
     def click_sendthrough(self):
         try:
-            self.click(self.lm.sms , self.lm.sms)
+            self.click(self.sm.sms , self.sm.sms)
         except Exception as e:
             logger.error(f"failed to click the send through: {str(e)}")
+    
+    def enter_msg(self, msg):
+        try:
+            self.send_keys(self.sm.message,msg)
+        except Exception as e:
+            logger.error(f"failed to enter message: {str(e)}")
 
+    def select_msgto(self):
+        try:
+            self.select_by_text(self.sm.dtr , self.sm.nurse)
+        except Exception as e:
+            logger.error(f"failed to select the message to: {str(e)}")        
+    
     def click_send(self):
         try:
-            self.click(self.lm.send)
+            self.click(self.sm.send)
         except Exception as e:
-            logger.error(f"failed to send the SMS: {str(e)}")
+            logger.error(f"failed to send the SMS: {str(e)}")    
 
     def get_success_msg(self):
         try:
