9bc61e0ae5c554be98ec26a3be4e81176bcc979d
Mythilysenthil <mythilsenthilp@gamil.com>
2026-06-15 11:02:39 +0530

    Added logger and tag

--- actions/send_msg_action.py
+++ actions/send_msg_action.py
@@ -15,65 +15,79 @@ class SendMsgAction(BaseAction):
     def click_msg(self):
         try:
             self.click(self.sp.messaging)
+            logger.info("Clicked Messaging menu successfully")
         except Exception as e:
             logger.error(f"failed to click the messaging menu: {str(e)}")
 
     def click_sendSMS(self):
         try:
             self.click(self.sm.sendSms)
+            logger.info("Clicked Messaging menu successfully")
         except Exception as e:
             logger.error(f"failed to click the send sms: {str(e)}")
 
     def enter_title(self,title):
         try:
             self.send_keys(self.sm.title,title)
+            logger.info(f"Entered title: {title}")
         except Exception as e:
             logger.error(f"failed to enter titel: {str(e)}")
 
     def enter_tempid(self, tempid):
         try:
             self.send_keys(self.sm.tempId,tempid)
+            logger.info(f"Entered template ID: {tempid}")
         except Exception as e:
             logger.error(f"failed to enter template id: {str(e)}")
 
     def click_sendthrough(self):
         try:
             self.click(self.sm.sms , self.sm.sms)
+            logger.info("Selected SMS as send through option")
         except Exception as e:
             logger.error(f"failed to click the send through: {str(e)}")
     
     def enter_msg(self, msg):
         try:
             self.send_keys(self.sm.message,msg)
+            logger.info("Entered SMS message successfully")
         except Exception as e:
             logger.error(f"failed to enter message: {str(e)}")
 
     def select_msgto(self):
         try:
             self.select_by_text(self.sm.dtr , self.sm.nurse)
+            logger.info("Selected Nurse from Message To dropdown")
         except Exception as e:
             logger.error(f"failed to select the message to: {str(e)}")        
     
     def click_send(self):
         try:
             self.click(self.sm.send)
+            logger.info("Clicked Send button successfully")
         except Exception as e:
             logger.error(f"failed to send the SMS: {str(e)}")    
 
     def get_success_msg(self):
         try:
-            return self.is_displayed(self.get_success_msg())
+            status = self.is_displayed(self.sm.success_msg)
+            logger.info("Success message displayed")
+            return status
         except Exception as e:
             logger.error(f"failed to displayed success msg: {str(e)}")
 
     def get_error_msg(self):
         try:
-            return self.is_displayed(self.get_error_msg())
+            status = self.is_displayed(self.sm.error_msg)
+            logger.info("Error message displayed")
+            return status
         except Exception as e:
             logger.error(f"failed to handle error msg: {str(e)}") 
 
     def get_missing_field_msg(self):
         try:
-            return self.is_displayed(self.get_missing_field_msg())
+            status = self.is_displayed(self.sm.missing_field_msg)
+            logger.info("Missing field validation message displayed")
+            return status
         except Exception as e:
             logger.error(f"failed to handle missing field error msg: {str(e)}")                                          
