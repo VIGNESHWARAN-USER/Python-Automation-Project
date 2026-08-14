bf8be42c537173d1c73e4f5db0817498c6610b41
Muhindhar <muhindharsv27@gmail.com>
2026-08-14 14:45:50 +0530

    Updated the appointment filter feature

--- pages/appointment_filter_pages.py
+++ pages/appointment_filter_pages.py
@@ -1,12 +1,12 @@
 from selenium.webdriver.common.by import By
 
 class Apponintmentfilter:
-    appointmentbtn = (By.XPATH,"//i[@class='fa fa-calendar-check-o']/following-sibling::span[text()='Appointment']")
-    todayapp = (By.XPATH,"//ul[@class='nav nav-tabs navlistscroll']/child::li[1]")
-    todayapptable = (By.XPATH,"//table[@id='DataTables_Table_0']")
-    upcomingapp = (By.XPATH,"//ul[@class='nav nav-tabs navlistscroll']/child::li[2]")
-    upcomingapptable = (By.XPATH,"//table[@id='DataTables_Table_1']/child::thead/child::tr/child::th[text()='Patient Name']")
+    appointmentbtn = (By.XPATH,"//li//a//span[text()='Appointment']")
+    todayapp = (By.XPATH,"//ul[contains(@class,'nav nav-pills sh-segmented-tabs')]//button[normalize-space()='Today Appointment']")
+    todayapptable = (By.XPATH,"//div[@class='card-header d-flex align-items-center justify-content-between flex-wrap gap-2 py-0 px-3']")
+    upcomingapp = (By.XPATH,"//ul[contains(@class,'nav nav-pills sh-segmented-tabs')]//button[normalize-space()='Upcoming Appointment']")
+    upcomingapptable = (By.XPATH,"//div[@class='card-header d-flex align-items-center justify-content-between flex-wrap gap-2 py-0 px-3']")
     #upcomingapptable = (By.XPATH,"//table[@id='DataTables_Table_1']")
-    oldapp = (By.XPATH,"//ul[@class='nav nav-tabs navlistscroll']/child::li[3]")
-    oldapptable = (By.XPATH,"//div[@id='DataTables_Table_2_wrapper']")
+    oldapp = (By.XPATH,"//ul[contains(@class,'nav nav-pills sh-segmented-tabs')]//button[normalize-space()='Old Appointment']")
+    oldapptable = (By.XPATH,"//div[@class='card-header d-flex align-items-center justify-content-between flex-wrap gap-2 py-0 px-3']")
     
